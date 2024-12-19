c0387e4f474d988930ea0735d107e45d111548cc
csallanm <asamelitonstem@gmail.com>
2024-12-19 12:09:33 +0800

    Add files via upload

---
+++ 404error.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=devicen-width, initial-scale=1.0">
+    <title>Page Not Found - Student Archiving System</title>
+    <link rel="icon" type="image/x-icon" href="assets/euC.png">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!--<link rel="stylesheet" href="customcolors.css"/>-->
+    <link rel="stylesheet" href="new_login.css" />
+    <link rel="stylesheet" href="effects.css" />
+    <link rel="stylesheet" href="appearance.css" />
+
+    <nav class="navbar fixed-top navbar-expand navbar-dark p-3" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0));;">
+        <!-- Navbar content -->
+        <a href="https://mseuf.edu.ph/candelaria" target="_blank"><img class="mx-3" src="assets/euC.png" alt="hugenerd" width="30" height="30"></a>
+        <ul class="navbar-nav nav-pills mr-auto">
+
+        </ul>
+    </nav>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+
+</head>
+
+<body>
+
+    <div class="bg">
+        <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
+            <div class="shadow-lg p-3 mb-4 rounded position-absolute" id="login_shape1">
+                <h3>Page Not Found</h3>
+                <p>The page that you entered isn't found.</p>
+
+                <a class="btn btn-secondary" href="homepage.php" id="btn-custom-color">Home</a>
+            </div>
+        </div>
+    </div>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+</body>
+
+</html>
